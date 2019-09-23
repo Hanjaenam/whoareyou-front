88babd1a0c7e4b58e07fc56ee69c8278471e06a1
한재남 <hanjn2842@naver.com>
2019-09-24 08:38:26 +0900

    t

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const isDev = process.env.NODE_ENV === 'development';
-const serverURL = isDev ? '/api' : 'https://52.78.130.165:443/api';
+const serverURL = isDev ? '/api' : 'https://52.78.130.165:80/api';
 
 export default ({ baseURL }: { baseURL: '/auth' | '/article' | '/user' }) =>
   axios.create({ baseURL: serverURL + baseURL });
